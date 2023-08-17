87bfeb0293839b980c72a9f942b0a59f834933ac
Shriya <shriya0690@gmail.com>
2023-08-17 23:46:14 +0530

    Styling changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
       <div>
       <h1 className='font-bold text-4xl mb-12 mt-6'>ALL CUSTOMERS</h1>
       {customers.map((customer) =>{
-        return <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow m-5" key={customer._id}>
+        return <div className="inline-block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow m-5" key={customer._id}>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{customer.name}</h5>
 
     <p className="mb-3 font-normal text-gray-700">{customer.email}.</p>
@@ -36,9 +36,10 @@ export default function Home() {
     <button onClick={() => dispatch(deleteCustomer(customer._id))} className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-base w-full sm:w-auto px-5 py-2.5 text-center m-1">Delete Customer</button>
 </div>
       })}
-
+      <div className='w-screen justify-center mt-10'>
       <button onClick={(e) => handleAddCustomer(e)} className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-base w-full sm:w-auto px-5 py-2.5 text-center m-1">Add Customer</button>
-
+      </div>
+    
       </div>
        
     </div>
