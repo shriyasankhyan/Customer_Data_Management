d67c5bbe9c6f8ed3bbc843cbf71d02d9ea5f9822
Shriya <shriya0690@gmail.com>
2023-08-18 15:03:39 +0530

    Extended true

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({extended : true}))
 app.use('/', Routes);
 
 
