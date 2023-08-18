00868008ea8495689f5c847d4822bfd24d71a32a
Shriya <shriya0690@gmail.com>
2023-08-18 15:05:59 +0530

    Backslash

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
-app.use('/', Routes);
+app.use('', Routes);
 
 
 const PORT = 8000;
